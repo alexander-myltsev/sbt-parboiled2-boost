3d3503d2686a3e80dd9c9b313f669c5b01a3adef
Alexander Myltsev <alexander.myltsev@phystech.edu>
2014-09-01 01:25:14 +0400

    Implement parboiled2 grammar

--- src/main/scala/Generate.scala
+++ src/main/scala/Generate.scala
@@ -5,12 +5,16 @@ import org.parboiled2._
 
 object Generator {
   def generateFromParboiledGrammar(template: String, expandTo: Int): String = {
-    //generate(TemplateParser.parse(template))(expandTo)
     val parser = new ParboiledParser(template)
     parser.InputLine.run() match {
-      case Success(exprAst)       => "Result: " + exprAst
-      case Failure(e: ParseError) => "Expression is not valid: " + parser.formatError(e)
-      case Failure(e)             => "Unexpected error during parsing run: " + e
+      case Success(exprAst) => 
+        s"""
+          |class ParboiledParser(val input: ParserInput) extends Parser {
+          |  ${exprAst.map(ParboiledParser.pretty).mkString("\n\n")}
+          |}
+        """.stripMargin
+      case Failure(e: ParseError) => throw new Exception("Expression is not valid: " + parser.formatError(e))
+      case Failure(e)             => throw new Exception("Unexpected error during parsing run: " + e)
     }
   }
 }
--- src/main/scala/ParboiledParser.scala
+++ src/main/scala/ParboiledParser.scala
@@ -4,35 +4,85 @@ import org.parboiled2._
 
 object ParboiledParser {
   sealed trait Expr
-  case class Value(value: String) extends Expr
-  case class Addition(lhs: Expr, rhs: Expr) extends Expr
-  case class Subtraction(lhs: Expr, rhs: Expr) extends Expr
-  case class Multiplication(lhs: Expr, rhs: Expr) extends Expr
-  case class Division(lhs: Expr, rhs: Expr) extends Expr
+  case class Sequence(lhs: Expr, rhs: Expr) extends Expr
+  case class FirstOf(lhs: Expr, rhs: Expr) extends Expr
+  case class Rule(name: Identifier, body: Expr) extends Expr
+  case class Identifier(name: String) extends Expr
+  case class StringLiteral(v: String) extends Expr
+  case class Optional(e: Expr) extends Expr
+  case class ZeroOrMore(e: Expr) extends Expr
+  case class OneOrMore(e: Expr) extends Expr
+  case class Test(e: Expr) extends Expr
+  case class Negate(e: Expr) extends Expr
+
+  def pretty(expr: Expr): String = expr match {
+    case Identifier(name: String) => name
+    case FirstOf(lhs, rhs) => pretty(lhs) + " | " + pretty(rhs)
+    case Sequence(lhs, rhs) => pretty(lhs) + " ~ " + pretty(rhs)
+    case Rule(name, body) => "def " + pretty(name) + " = " + pretty(body)
+    case Optional(e) => "optional(" + pretty(e) + ")"
+    case ZeroOrMore(e) => "zeroOrMore(" + pretty(e) + ")"
+    case OneOrMore(e) => "oneOrMore(" + pretty(e) + ")"
+    case StringLiteral(v) => '"' + v + '"'
+    case Test(e) => "&(" + pretty(e) + ")"
+    case Negate(e) => "!(" + pretty(e) + ")"
+  }
 }
 
 class ParboiledParser(val input: ParserInput) extends Parser {
   import ParboiledParser._
+
+  import CharPredicate.{Alpha, AlphaNum, Visible}
   
   def InputLine = rule { Expression ~ EOI }
 
-  def Expression: Rule1[Expr] = rule {
-    Term ~ zeroOrMore(
-      '+' ~ Term ~> Addition
-        | '-' ~ Term ~> Subtraction)
+  def Expression = rule { zeroOrMore(PbRule ~ WhiteSpace) }
+  
+  def PbRule = rule { "def" ~ Ident ~ "=" ~ Body ~> Rule }
+  
+  def Body: Rule1[Expr] = rule { SeqRule ~ zeroOrMore("|" ~ SeqRule ~> FirstOf) }
+  
+  def SeqRule: Rule1[Expr] = rule { SimpleExpr ~ zeroOrMore("~" ~ SimpleExpr ~> Sequence) }
+  
+  def SimpleExpr: Rule1[Expr] = rule { TestRule | NegateRule | OneOrMoreRule | ZeroOrMoreRule | 
+                                       OptionalRule | StringLiteralRule | Ident | ('(' ~ Body ~ ')') }
+  
+  def OptionalRule = rule { WhiteSpace ~ "optional(" ~ Body ~ ")" ~ WhiteSpace ~> Optional }
+  
+  def ZeroOrMoreRule = rule { WhiteSpace ~ "zeroOrMore(" ~ Body ~ ")" ~ WhiteSpace ~> ZeroOrMore }
+  
+  def OneOrMoreRule = rule { WhiteSpace ~ "oneOrMore(" ~ Body ~ ")" ~ WhiteSpace ~> OneOrMore }
+  
+  def StringLiteralRule = rule { WhiteSpace ~ '"' ~ capture(zeroOrMore(AlphaNum)) ~ '"' ~ WhiteSpace ~> StringLiteral }
+  
+  def TestRule = rule { WhiteSpace ~ "&(" ~ Body ~ ")" ~ WhiteSpace ~> Test }
+  
+  def NegateRule = rule { WhiteSpace ~ "!(" ~ Body ~ ")" ~ WhiteSpace ~> Negate }
+  
+  def Ident = rule { WhiteSpace ~ capture(Alpha ~ zeroOrMore(AlphaNum)) ~ WhiteSpace ~> Identifier }
+
+  def WhiteSpace = rule { zeroOrMore(WhiteSpaceChar) }
+  
+  val WhiteSpaceChar = CharPredicate(" \n\r\t\f")
+}
+
+object ParboiledParserTest {  
+  import scala.util.{Failure, Success}
+  import org.parboiled2._
+
+  def parse(input: String) = {
+    val parser = new ParboiledParser(input)
+    println("-----")
+    parser.InputLine.run() match {
+      case Success(exprAst)       => println("Result:\n" + exprAst.map(ParboiledParser.pretty).mkString("\n"))
+      case Failure(e: ParseError) => println("Expression is not valid: " + parser.formatError(e))
+      case Failure(e)             => println("Unexpected error during parsing run: " + e)
+    }
   }
-
-  def Term = rule {
-    Factor ~ zeroOrMore(
-      '*' ~ Factor ~> Multiplication
-        | '/' ~ Factor ~> Division)
+  
+  def main(args: Array[String]): Unit = {
+    parse(
+      """def a = !(oneOrMore(abc) ~ edf | k) ~ optional(edf)
+        |""".stripMargin)
   }
-
-  def Factor = rule { Number | Parens }
-
-  def Parens = rule { '(' ~ Expression ~ ')' }
-
-  def Number = rule { capture(Digits) ~> Value }
-
-  def Digits = rule { oneOrMore(CharPredicate.Digit) }
 }
